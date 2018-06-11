79995f5e7ce4a4e99a23b75a796836a618d1db5c
Ramya Rao <ramrao@umail.iu.edu>
2018-06-10 21:26:27 -0400

    changing the handler from defaultServeMux to a different mux initialized in this program

--- src/TcpComm/httpRevision/main.go
+++ src/TcpComm/httpRevision/main.go
@@ -6,14 +6,32 @@ import (
 	"strings"
 )
 
-func main() {
-	http.HandleFunc("/", sroot)
+type dogHandler int
 
-	http.ListenAndServe(":9000", nil)
+type catHandler int
+
+/*
+The main function initializes a multiplexer, which routes
+/dog/ patter URL to dog handler and
+/cat/ patter URL to cat handler
+*/
+func main() {
+
+	var dog dogHandler
+	var cat catHandler
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/dog/", dog)
+	mux.Handle("/cat/", cat)
+
+	// The server listens and serves all requests coming to to locahost:9000
+	// using mux as the multiplexer
+	http.ListenAndServe(":9000", mux)
 
 }
 
-func sroot(response http.ResponseWriter, request *http.Request) {
+func (d dogHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	fs := strings.Split(request.URL.Path, "/")
@@ -26,5 +44,19 @@ func sroot(response http.ResponseWriter, request *http.Request) {
 
 		io.WriteString(response, `<strong> Welcome to this webpage </strong>`)
 	}
+}
 
+func (c catHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	fs := strings.Split(request.URL.Path, "/")
+	if len(fs) >= 3 {
+		dogname := fs[2]
+		io.WriteString(response, `<strong> Welcome to this webpage </strong> with dog name as `+dogname+`</br>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/0/06/Kitten_in_Rizal_Park%2C_Manila.jpg">
+			`)
+	} else {
+
+		io.WriteString(response, `<strong> Welcome to this webpage through cat handler </strong>`)
+	}
 }
